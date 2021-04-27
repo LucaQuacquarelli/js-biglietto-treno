81b8135c6ad40a3073d51ef4ecbdfb4f5f9e7ad2
LucaQuacquarelli <lucaq82@libero.it>
2021-04-27 16:34:24 +0200

    layout

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var yourAge = prompt("Quanti anni hai?");
+document.getElementById("age").innerHTML = yourAge + " Anni";
+var travel = prompt("Quanti KM vuoi percorrere?");
+document.getElementById("km").innerHTML = travel + " Km";
