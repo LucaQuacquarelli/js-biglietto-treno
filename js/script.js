34f0ac259b044e10cc0e47199fb0e4c1ea9fa2c8
LucaQuacquarelli <lucaq82@libero.it>
2021-04-27 17:09:28 +0200

    Calcolo Prezzi

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+// Chiedee all'utente quanti anni ha
 var yourAge = prompt("Quanti anni hai?");
 document.getElementById("age").innerHTML = yourAge + " Anni";
+
+// Chiedere all'utente quanti km vuole percorrere
 var travel = prompt("Quanti KM vuoi percorrere?");
 document.getElementById("km").innerHTML = travel + " Km";
+
+// Calcolo del costo del biglietto
+var ticketPrice = (travel * 0.21);
+document.getElementById("price").innerHTML = ticketPrice + " &euro;";
+
+// Sconto
+if (yourAge < 18) {
+    var youngster = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
+    document.getElementById("price").innerHTML = youngster + " &euro;";
+} else (yourAge > 65); {
+    var senior = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
+    document.getElementById("price").innerHTML = senior + " &euro;";
+}
