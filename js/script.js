ecc4d9b0a26506e0a568e7f4939f1365d171fa29
LucaQuacquarelli <lucaq82@libero.it>
2021-04-28 10:14:02 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Chiedee all'utente quanti anni ha
-var yourAge = prompt("Quanti anni hai?");
+var yourAge = parseInt(prompt("Quanti anni hai?"));
 document.getElementById("age").innerHTML = yourAge + " Anni";
 
 // Chiedere all'utente quanti km vuole percorrere
-var travel = prompt("Quanti KM vuoi percorrere?");
+var travel = parseInt(prompt("Quanti KM vuoi percorrere?"));
 document.getElementById("km").innerHTML = travel + " Km";
 
 // Calcolo del costo del biglietto
@@ -11,10 +11,11 @@ var ticketPrice = (travel * 0.21);
 document.getElementById("price").innerHTML = ticketPrice + " &euro;";
 
 // Sconto
+var sconto = 0;
 if (yourAge < 18) {
-    var youngster = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
-    document.getElementById("price").innerHTML = youngster + " &euro;";
-} else (yourAge > 65); {
-    var senior = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
-    document.getElementById("price").innerHTML = senior + " &euro;";
+    sconto = (ticketPrice - ((ticketPrice * 20) / 100)).toFixed(2);
+    document.getElementById("price").innerHTML = sconto + " &euro;";
+} else if (yourAge >= 65) {
+    sconto = (ticketPrice - ((ticketPrice * 40) / 100)).toFixed(2);
+    document.getElementById("price").innerHTML = sconto + " &euro;";
 }
